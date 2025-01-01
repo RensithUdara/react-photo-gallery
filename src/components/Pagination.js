2619f0b7e83d57ca9cd00e80c7aaad12eae20829
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-01-01 21:17:58 +0530

    improve page navigate function

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,28 +1,38 @@
-import React from 'react'
-import { useDispatch } from 'react-redux';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { nextPage, previousPage } from '../redux/gallery/galleryslice';
 
 function Pagination() {
-
     const dispatch = useDispatch();
+    const page = useSelector((state) => state.gallery.page);
 
     const handleNext = () => {
         dispatch(nextPage());
-    }
+    };
 
     const handlePrevious = () => {
-        dispatch(previousPage());
-    }
+        if (page > 1) {
+            dispatch(previousPage());
+        }
+    };
+
     return (
-        <div>
-            <button className='bg-blue-500 text-white px-4 py-2 rounded-lg'
+        <div className="flex justify-center gap-4 mt-6">
+            <button
+                className="bg-blue-500 text-white px-4 py-2 rounded-lg disabled:opacity-50 disabled:cursor-not-allowed transition hover:bg-blue-600"
                 onClick={handlePrevious}
-            >Previous</button>
-            <button className='bg-blue-500 text-white px-4 py-2 rounded-lg'
+                disabled={page === 1}
+            >
+                Previous
+            </button>
+            <button
+                className="bg-blue-500 text-white px-4 py-2 rounded-lg transition hover:bg-blue-600"
                 onClick={handleNext}
-            >Next</button>
+            >
+                Next
+            </button>
         </div>
-    )
+    );
 }
 
-export default Pagination
+export default Pagination;
