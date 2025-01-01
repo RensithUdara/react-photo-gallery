e500e1cc05da2cf657b9a696c88b12a5b1dcbca7
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-01-01 21:18:38 +0530

    improve theme slice

--- src/redux/theme/themeSlice.js
+++ src/redux/theme/themeSlice.js
@@ -1,17 +1,16 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 const themeSlice = createSlice({
     name: 'theme',
     initialState: {
-        darkMode: false
+        darkMode: false,
     },
     reducers: {
-        toggleTheme: (state ) => {
+        toggleTheme: (state) => {
             state.darkMode = !state.darkMode;
-        }
-    }
+        },
+    },
 });
 
 export const themeReducer = themeSlice.reducer;
-
-export const {toggleTheme} = themeSlice.actions;
+export const { toggleTheme } = themeSlice.actions;
