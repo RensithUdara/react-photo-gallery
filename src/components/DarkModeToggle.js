fe300176a4f0c88f323027846a16eed7d87446d1
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-01-01 21:16:46 +0530

    improve darkmode function

--- src/components/DarkModeToggle.js
+++ src/components/DarkModeToggle.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux'; // Added useDispatch import
-import { toggleTheme } from '../redux/theme/themeSlice'; // Adjust the path accordingly
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleTheme } from '../redux/theme/themeSlice';
 
 function DarkModeToggle() {
     const darkMode = useSelector((state) => state.theme.darkMode);
@@ -12,9 +12,9 @@ function DarkModeToggle() {
 
     return (
         <div
-            className={`flex cursor-pointer  gap-3 font-mono border p-3 rounded-lg justify-center items-center ${darkMode ? "bg-black text-white border-white" : "bg-white text-black border-black"
-                }`}
-            onClick={handleClick} // Simplified the onClick function call
+            className={`flex cursor-pointer gap-3 font-mono border p-3 rounded-lg justify-center items-center transition duration-300 transform hover:scale-105 ${darkMode ? "bg-black text-white border-white" : "bg-white text-black border-black"}`}
+            title={`Switch to ${darkMode ? "Light Mode" : "Dark Mode"}`}
+            onClick={handleClick}
         >
             <div>{darkMode ? "Dark Mode" : "Light Mode"}</div>
             <div className="text-2xl">{darkMode ? "🌚" : "🌝"}</div>
